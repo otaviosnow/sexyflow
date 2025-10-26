711354bc0902f93cac1d4f264935eb38715dd551
otaviosnow <otaviosnow@github.com>
2025-10-26 06:35:47 -0300

    fix: corrigir importaÃ§Ãµes connectDB e remover appDir deprecated

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { connectDB } from './db'
+import connectDB from './db'
 import User from '../models/User'
 import bcrypt from 'bcryptjs'
 
