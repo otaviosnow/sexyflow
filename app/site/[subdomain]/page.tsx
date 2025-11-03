f4592ac9f4bc428109e7ad6b3739ffbf98975329
Otavio Henrique <otaviosnow@gmail.com>
2025-11-03 08:18:38 -0300

    Adicionar import do PageRenderer que estava faltando no arquivo page.tsx

--- app/site/[subdomain]/page.tsx
+++ app/site/[subdomain]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from 'next/navigation';
 import connectDB from '@/lib/db';
 import Project from '@/models/Project';
 import Page from '@/models/Page';
+import PageRenderer from '@/components/PageRenderer';
 
 interface SubdomainPageProps {
   params: {
